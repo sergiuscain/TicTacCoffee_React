3b9b6d1236f57fcce818679fe01b06d843787ae9
sergiuscain <sergiussmithe@gmail.com>
2025-02-04 15:06:51 +0300

    Поменял нолик на изображение Нолика из фиксиков

--- tictactoecoffee_react/src/App.jsx
+++ tictactoecoffee_react/src/App.jsx
@@ -1,23 +1,25 @@
-﻿/*import { useState } from 'react'*/
-import * as React from 'react';
-import './App.css'
+﻿import * as React from 'react';
+import './App.css';
 import { useState } from 'react';
 
 function App() {
-
-  return (
-      <Game/>
-  )
+    return <Game />;
 }
 
-export default App
+export default App;
 
-const SYMBOL_X = `☕`;
-const SYMBOL_O = `O`
+const SYMBOL_X = `☕`; // Латте
+const SYMBOL_O = (
+    <img
+        src="https://gas-kvas.com/grafic/uploads/posts/2023-10/1696531355_gas-kvas-com-p-kartinki-nolik-2.png"
+        alt="Нолик"
+        className="icon"
+    />
+); // Нолик в виде изображения
 
 function Game() {
-    const [cells, setCells ] = useState([null, null, null, null, null, null, null, null, null]);
-    const [currentStep, setCurrentStep ] = useState(SYMBOL_O);
+    const [cells, setCells] = useState([null, null, null, null, null, null, null, null, null]);
+    const [currentStep, setCurrentStep] = useState(SYMBOL_O);
 
     const handleCellClick = (index) => {
         if (cells[index]) {
@@ -27,15 +29,22 @@ function Game() {
         cellsCopy[index] = currentStep;
         setCells(cellsCopy);
         setCurrentStep(currentStep === SYMBOL_O ? SYMBOL_X : SYMBOL_O);
-    }
+    };
 
-    return <div className="container">
-        <h1>Кофе-Нолики</h1>
-        <h2>Ход: {currentStep }</h2>
-        <div className="grid">
-            {cells.map((symbol, index )=> {
-                return <button className="cell" key={index} onClick={() => handleCellClick(index) }>{symbol} </button>
-            })}
+    return (
+        <div className="container">
+            <h1>Кофе-Нолики</h1>
+            <h2>Ход: {currentStep === SYMBOL_O ? 'Нолик' : 'Латте'}</h2>
+            <div className="grid">
+                {cells.map((symbol, index) => {
+                    return (
+                        <button className="cell" key={index} onClick={() => handleCellClick(index)}>
+                            {symbol}
+                        </button>
+                    );
+                })}
+            </div>
         </div>
-    </div>
+    );
 }
+
