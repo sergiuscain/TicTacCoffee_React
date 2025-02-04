ad31d56186a4d1369c6135da38f3f94be6f5389e
sergiuscain <sergiussmithe@gmail.com>
2025-02-04 13:52:23 +0300

    Создал массив поля и сделал отрисовку поля в зависимости от этого массива. И вынес переменные крестика, нолика и текущего хода.

--- tictactoecoffee_react/src/App.jsx
+++ tictactoecoffee_react/src/App.jsx
@@ -4,21 +4,25 @@ import './App.css'
 function App() {
 
   return (
-      <div className="container">
-          <h1>Крестики-Нолики</h1>
-          <div className="grid">
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-              <button className="cell">☕</button>
-          </div>
-      </div>
+      <Game/>
   )
 }
 
 export default App
+
+const SYMBOL_X = `☕`;
+const SYMBOL_O = `O`
+function Game() {
+    const cells = [null, null, null, SYMBOL_O,SYMBOL_X, SYMBOL_O, null, null, SYMBOL_X];
+    const currentStep = SYMBOL_O;
+
+    return <div className="container">
+        <h1>Кофе-Нолики</h1>
+        <h2>Ход: {currentStep }</h2>
+        <div className="grid">
+            {cells.map((symbol, index )=> {
+                return <button className="cell" key={index}>{symbol} </button>
+            })}
+        </div>
+    </div>
+}
