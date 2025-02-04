f4d5ccc8098fd089b8bec0124f93e94cf0de9dc5
sergiuscain <sergiussmithe@gmail.com>
2025-02-04 14:40:08 +0300

    Добавил логику переключения хода и заполнения поля с помощью useState

--- tictactoecoffee_react/src/App.jsx
+++ tictactoecoffee_react/src/App.jsx
@@ -1,5 +1,7 @@
 ﻿/*import { useState } from 'react'*/
+import * as React from 'react';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
@@ -12,16 +14,27 @@ export default App
 
 const SYMBOL_X = `☕`;
 const SYMBOL_O = `O`
+
 function Game() {
-    const cells = [null, null, null, SYMBOL_O,SYMBOL_X, SYMBOL_O, null, null, SYMBOL_X];
-    const currentStep = SYMBOL_O;
+    const [cells, setCells ] = useState([null, null, null, null, null, null, null, null, null]);
+    const [currentStep, setCurrentStep ] = useState(SYMBOL_O);
+
+    const handleCellClick = (index) => {
+        if (cells[index]) {
+            return;
+        }
+        const cellsCopy = cells.slice();
+        cellsCopy[index] = currentStep;
+        setCells(cellsCopy);
+        setCurrentStep(currentStep === SYMBOL_O ? SYMBOL_X : SYMBOL_O);
+    }
 
     return <div className="container">
         <h1>Кофе-Нолики</h1>
         <h2>Ход: {currentStep }</h2>
         <div className="grid">
             {cells.map((symbol, index )=> {
-                return <button className="cell" key={index}>{symbol} </button>
+                return <button className="cell" key={index} onClick={() => handleCellClick(index) }>{symbol} </button>
             })}
         </div>
     </div>
