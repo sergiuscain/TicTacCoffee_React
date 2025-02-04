319a2e5036627a73b57a35673917cfec02575b13
sergiuscain <sergiussmithe@gmail.com>
2025-02-04 12:02:52 +0300

    Добавьте файлы проекта.

---
+++ tictactoecoffee_react/src/App.jsx
@@ -0,0 +1,24 @@
+﻿/*import { useState } from 'react'*/
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="container">
+          <h1>Крестики-Нолики</h1>
+          <div className="grid">
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+              <button className="cell">☕</button>
+          </div>
+      </div>
+  )
+}
+
+export default App
